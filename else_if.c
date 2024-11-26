b117364c0f83c5a9e3420b837e533c308c692ff2
Ananya <ananyakhananya@gmail.com>
2024-11-26 06:10:39 -0800

    Add files via upload

---
+++ else_if.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main() {
+ int a,b,c;
+ printf("enter three numbers:");
+ scanf("%d%d%d",&a,&b,&c);
+if (a>b && a>c) {
+  printf("largest number is %d\n",a);
+  } esle if (b>c) {
+  printf("largest number is %d\n",b);
+  } else {
+  printf("largest number is %d\n",c);
+  }
+  printf("Ananya:");
+  return 0;
+} 
