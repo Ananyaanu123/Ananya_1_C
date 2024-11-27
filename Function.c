9d585f2bd4d7fafcaf4265ee90a8d6d5761f854e
Ananya <ananyakhananya@gmail.com>
2024-11-27 10:28:55 +0530

    Function.c

---
+++ Function.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int add(int a, int b) {
+    return a+b;
+}
+
+int main() {
+    int result = add(5,10);
+    printf("Sum=%d\n", result);{
+        printf("Ananya");
+    }
+    return 0;
+}
